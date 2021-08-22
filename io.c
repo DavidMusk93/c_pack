56ca85d09896a2613fc9b932c5d62ebb8485c400
Mingqiang Sun <davidmusksun1993@gmail.com>
2021-08-22 19:52:42 +0800

    store(load) integer to(from) char array
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- io.c
+++ io.c
@@ -17,3 +17,20 @@ void unpacker_init(UNPACKER*self,i8*s,i8*e){
     self->s=s,self->e=e;
     self->unpack=&unpack;
 }
+
+#if TEST_CONVERT
+#include "macro.h"
+#include "log.h"
+
+typedef char a8_t[8];
+
+MAIN(){
+    u64 x=0x1122334455667788,y;
+    a8_t a;
+    char*p=&a[0];
+    u64toc(x,p);
+    p=&a[0];
+    c2u64(p,y);
+    LOGINFO("%lx",y);
+}
+#endif
--- io.h
+++ io.h
@@ -3,6 +3,52 @@
 
 #include "type.h"
 
+#define __lc(t,c) ((t)(u8)*((c)++))
+#define c2u16(c,u) (\
+u=__lc(u16,c),\
+u|=__lc(u16,c)<<8\
+)
+#define c2u32(c,u) (\
+u=__lc(u32,c),\
+u|=__lc(u32,c)<<8,\
+u|=__lc(u32,c)<<16,\
+u|=__lc(u32,c)<<24\
+)
+#define c2u64(c,u) (\
+u=__lc(u64,c),\
+u|=__lc(u64,c)<<8,\
+u|=__lc(u64,c)<<16,\
+u|=__lc(u64,c)<<24,\
+u|=__lc(u64,c)<<32,\
+u|=__lc(u64,c)<<40,\
+u|=__lc(u64,c)<<48,\
+u|=__lc(u64,c)<<56\
+)
+
+#define __sc(c,x) *((c)++)=(u8)((x)&0xff)
+#define u16toc(u,c) (\
+__sc(c,u),\
+__sc(c,(u)>>8)\
+)
+//#define u32toc(u,c) u16toc(u,c),u>>=16,u16toc(u,c)
+//#define u64toc(u,c) u32toc(u,c),u>>=32,u32toc(u,c)
+#define u32toc(u,c) (\
+__sc(c,u),\
+__sc(c,(u)>>8),\
+__sc(c,(u)>>16),\
+__sc(c,(u)>>24)\
+)
+#define u64toc(u,c) (\
+__sc(c,u),\
+__sc(c,(u)>>8),\
+__sc(c,(u)>>16),\
+__sc(c,(u)>>24),\
+__sc(c,(u)>>32),\
+__sc(c,(u)>>40),\
+__sc(c,(u)>>48),\
+__sc(c,(u)>>56)\
+)
+
 struct st_unpacker_t{
     i8*s;
     i8*e;
